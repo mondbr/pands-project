d7d387e7f7835517a2325ab682e901e0d24483df
mondbr <mondab133@gmail.com>
2024-05-08 21:35:56 +0100

    writing stat values and info to txt file

--- analysis.py
+++ analysis.py
@@ -23,14 +23,38 @@ df = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/
 # Checking the output of the dataset
 print(df)
 
+# Checking the types of variables in dataset
+print (df.info())
 
 # Output a summary of each variable to a single text file
-## 
+## Render a DataFrame to a console-friendly tabular output:
+## https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.to_string.html
+dscr = df.describe().to_string()
+
+##https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.info.html
+
+
+
+# creating a file
 FILENAME = 'summary_analysis.txt'
 with open(FILENAME, 'w+t') as f:
-    for line in f:
-        print (line)
-    print ("Checking first line")
+    f.write('\n')
+    f.write('### SUMMARY OF THE IRIS DATASET ###\n')
+    f.write('\n')
+    f.write('Iris dataset basic statistical values:\n')
+    f.write('=============================================================')
+    f.write('\n')
+    f.write(dscr)
+    f.write('\n')
+    f.write('=============================================================')
+    f.write('\n\n')
+    f.write('Number of samples of each type and variable type:\n')
+    f.write('=============================================================')
+    f.write('\n')
+                   # how to save output from dataframe info to text file
+    df.info(buf=f) # https://stackoverflow.com/questions/35436331/how-to-save-output-from-dataframe-info-to-file-a-excel-or-text-file
+    f.write('=============================================================')
+    f.write('\n\n')
 
 
 
